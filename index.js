2cda3e84af6a246e4ae00a7b4e07d6dc8d5e6d13
Bruce Pereira <bloomdev90@gmail.com>
2024-05-14 19:11:54 -0400

    first

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-console.log('hello world!');
+// TIPOS DE DATOS
+//
+// a = "test"
+//
+// console.log(a);
+//
+// persona = {
+//     numeroDePiernas: 4,
+//     colorDeOjos: 'rojo'
+// };
+//
+// console.log(persona.colorDeOjos) -> 'rojo'
+// 
+// list = ['bob, 'jill', 'dictto'];
+//
+// console.log(list[0]) -> 'bob'
+// console.log(list[1]) -> 'jill'
+//
+// a == b -> una comparacion, el resultado es uno de verdad O falso
+//
+// a = 0;
+// test = a == 5;
+//
+//
+//
+// CONDITIONALS
+//
+// if (test) {
+//     a = 1;
+// }
+// else {
+//     a = 2;
+// }
+// 
+// console.log(a); -> 2
+//
+//
+// LOOPS
+//
+// a = 0;
+// while (a < 10) {
+//     a = a + 2;
+// }
+// console.log(a);
+//
+// for (i = 0; i < list.length; i = i + 2) {
+//     console.log(list[1]);
+// }
+// console.log(i);
+//
+//
+
+
